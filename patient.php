5d7a1d87518bfccd0408d27930b3376251d184f0
GiftyMateKole <giftym94@gmail.com>
2016-03-18 12:32:56 +0000

    Update patient.php

--- patient.php
+++ patient.php
@@ -1,13 +1,25 @@
+
 <?php
 
-include_once "adb.php";
+include_once "adb_1.php";
 
-class patient extends adb{
+class patient extends adb_1{
 	function users(){
 	}
-//Function to add patients to the database
+	/**
+	*Adds a new patient
+	*@param int $id student id
+	*@param string $firstname first name
+	*@param string $lastname last name
+	*@param int $age student age
+	*@param string $email student email
+	*@param string $location student hotel
+	*@param int $insurance student insurance id
+	*@return boolean returns true if successful or false 
+	*/
 	function addPatient($id,$fname,$lname,$age,$gender,$num,$email,$location,$insurance){
-		$query="insert into patient set STUDENT_ID='$id',FIRSTNAME='$fname',LASTNAME='$lname',PHONE='$num',EMAIL='$email',AGE='$age',GENDER='$gender',LOCATION='$location',INSURANCE_ID='$insurance'";
+		$query="insert into patient set STUDENT_ID='$id',FIRSTNAME='$fname',LASTNAME='$lname',PHONE='$num',EMAIL='$email',AGE='$age',
+		        GENDER='$gender',LOCATION='$location',INSURANCE_ID='$insurance'";
 		$result=$this->query($query);
 		if($result){
 		echo "Patient Added Successfully";
@@ -17,8 +29,9 @@ class patient extends adb{
 		return $result;
 	}
 }
-/*
-$obj=new clinic();
+
+/**
+$obj=new patient();
 $obj->addPatient('23456789','Elsa','Frozen',20,'FEMALE',23456789,'elsa@gmail.com','ON-CAMPUS',24345668);
 */
 ?>
