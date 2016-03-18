d06c2b8a23c9443c276752a9e664e5cf12dc857c
JessicaAnkoh <jessica.ankoh@ashesi.edu.gh>
2016-03-18 15:01:59 +0000

    Update deletePatient.php

--- deletePatient.php
+++ deletePatient.php
@@ -8,7 +8,11 @@ include_once("Adm.php");
 class Info extends Adm{
 	function Info(){
 	}
-
+	/**
+	*Function get patient
+	*@param int $filter which stores student id
+	*@param boolean returns true if successful and false when there is failure
+	*/
 	function getPatient($filter = false){
 		$strQuery="select STUDENT_ID,FIRSTNAME,LASTNAME,GENDER,AGE,PHONE,EMAIL,INSURANCE_ID,LOCATION from patient";
 		if ($filter!=false){
@@ -16,6 +20,10 @@ class Info extends Adm{
 		}
 		return $this->query($strQuery);
 	}
+	/**
+	*Searchs for patient using student id
+	*@param int $text contains the student id 
+	*/
 		function searchUsers($text=false){
 		$filter=false;
 		if($text!=false){
@@ -25,7 +33,10 @@ class Info extends Adm{
 		return $this->getPatient($filter);
 	}
 	
-	
+	/**
+	*Deletes information on patients
+	*@param int $pid stores and calls the patient's student id whose record is to be deleted
+	*/
 	function deletePatient($pid){
 		$strQuery = "delete from patient where STUDENT_ID = $pid";
 		return $this->query($strQuery);
