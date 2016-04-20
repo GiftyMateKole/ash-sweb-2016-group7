254eecdffbc63e2727fc9756ace19a369f97d644
JessicaAnkoh <jessica.ankoh@ashesi.edu.gh>
2016-04-20 02:55:58 +0100

    Update deletepage.php

--- deletepage.php
+++ deletepage.php
@@ -4,7 +4,7 @@
 	//include users.php
 	include_once("User.php");
 	
-	//check for user code
+	//check for user code(uc) to delete from patient table
 	if(isset($_REQUEST['uc'])){
 	$pid = $_REQUEST['uc'];
 	
@@ -16,24 +16,26 @@
 		exit();
 	}
 	
+	//check for user code(un) to delete from nurse table
 	if(isset($_REQUEST['un'])){
 	$nid = $_REQUEST['un'];
 	$newDel2 = new Info();
 	$Del2 = $newDel2->deleteNurse($nid);
 	if ($Del2 == false){
-		
+		echo "error";	
 	}
-	echo "error";
+	
 		exit();
 	}
 	
+	//check for user code(ud) to delete from diagnosis table
 	if(isset($_REQUEST['ud'])){
 	$did = $_REQUEST['ud'];
 	$newDel3 = new Info();
 	$Del3 = $newDel3->deleteDiagnosis($did);
-	if ($Del3 == false){	
+	if ($Del3 == false){
+		echo "error";
 	}
-	echo "error";
 		exit();
 	}
 	?>
