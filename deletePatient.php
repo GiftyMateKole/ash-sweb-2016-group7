4e8f4c9e18c17d8313ffe924638ef176a548282c
JessicaAnkoh <jessica.ankoh@ashesi.edu.gh>
2016-04-20 03:04:18 +0100

    Update deletePatient.php

--- deletePatient.php
+++ deletePatient.php
@@ -20,6 +20,15 @@ class Info extends Adm{
 		}
 		return $this->query($strQuery);
 	}
+	
+	//method to retrieve the nurse information
+	function getNurse($filter = false){
+		$strQuery="select NURSE_ID,FIRST_NAME,LAST_NAME,EMAIL,PHONE,USERNAME,PWORD from nurse";
+		if ($filter!=false){
+			$strQuery = $strQuery."where $filter";
+		}
+		return $this->query($strQuery);
+	}
 	/**
 	*Searchs for patient using student id
 	*@param int $text contains the student id 
@@ -33,12 +42,27 @@ class Info extends Adm{
 		return $this->getPatient($filter);
 	}
 	
+	//search for nurse using nurse id
+	function searchNurse($text=false){
+		$filter=false;
+		if($text!=false){
+			$filter=" NURSE_ID = $text";
+		}
+	
+		return $this->getNurse($filter);
+	}
 	/**
 	*Deletes information on patients
 	*@param int $pid stores and calls the patient's student id whose record is to be deleted
 	*/
 	function deletePatient($pid){
 		$strQuery = "delete from patient where STUDENT_ID = $pid";
+		return $this->query($strQuery);
+	}
+	
+	//method to delete nurse information
+	function deleteNurse($nid){
+		$strQuery = "delete from nurse where NURSE_ID = $nid";
 		return $this->query($strQuery);
 	}
 }
