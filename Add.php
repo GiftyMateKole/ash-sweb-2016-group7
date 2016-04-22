17cead31764dec9cbf5e2e2e6aa54e129e967534
GiftyMateKole <giftym94@gmail.com>
2016-04-22 17:22:35 +0100

    Update Add.php
    function add patient()

--- Add.php
+++ Add.php
@@ -12,6 +12,23 @@
 				}
 				divStatus.innerHTML=xhr.responseText;
 			}
+			function addPatient(){
+				var id = document.getElementsByName("student_id")[0].value;
+				var fname = document.getElementsByName("fname")[0].value;
+				var lname = document.getElementsByName("lname")[0].value;
+				var age = document.getElementsByName("age")[0].value;
+				var gender = document.getElementsByName("gender")[0].value;
+				var location = document.getElementsByName("location")[0].value;
+				var phone = document.getElementsByName("num")[0].value;
+				var email = document.getElementsByName("email")[0].value;
+				var insurance = document.getElementsByName("insurance")[0].value;
+				
+				var ajaxPageUrl="patientAjax.php?cmd=1&uc="+id+"&fname="+fname+"&lname="+lname+"&age="+age+
+				"&gender="+gender+"&location="+location+"&phone="+phone+"&email="+email+"&insurance="+insurance;
+				$.ajax(ajaxPageUrl,
+{async:true,complete:addPatientComplete	}	
+				);
+			}
 			</script>
 			</head>
 			</html>
